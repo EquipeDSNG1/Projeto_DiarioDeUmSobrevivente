b6e2df64edcf184591be7fbad7c77a932c2f3bfa
kennedyEmanoel <79938217+kennedyEmanoel@users.noreply.github.com>
2021-11-29 11:12:27 -0300

    shop and inventory

--- Codigo/Assets/Inventory/Script/Player.cs
+++ Codigo/Assets/Inventory/Script/Player.cs
@@ -5,33 +5,28 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     static public Inventory _inventoryPlayer;
-    static public bool _pause;
+    static public bool _pause = false;
     public GameObject inventory;
     GameObject[] menus;
-    GameObject[] shop;
     void Start()
     {
         _inventoryPlayer = inventory.GetComponent<Inventory>();
         menus = GameObject.FindGameObjectsWithTag("Inventory");
-        shop = GameObject.FindGameObjectsWithTag("Shop");
+        OnOffInventory();
     }
 
-    // Update is called once per frame
+    void OnOffInventory()
+    {
+        for (int i = 0; i < menus.Length; i++)
+            menus[i].SetActive(_pause);
+    }
+
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.T))
         {
             _pause =! _pause;
-            
-            for (int i = 0; i < menus.Length; i++)
-                menus[i].SetActive(_pause);
-        }
-        if(Input.GetKeyDown(KeyCode.G))
-        {
-            _pause =! _pause;
-            
-            for (int i = 0; i < shop.Length; i++)
-                shop[i].SetActive(_pause);
+            OnOffInventory();
         }
     }
 }
